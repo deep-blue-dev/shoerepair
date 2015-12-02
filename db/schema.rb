90bb1a987d82c19f95783fe35f16417253b1545d
Hector <hmgaar@gmail.com>
2015-12-01 19:53:12 -0500

    added position to image model, pages/index.html has backbone

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201210136) do
+ActiveRecord::Schema.define(version: 20151202004805) do
 
   create_table "images", force: :cascade do |t|
     t.string   "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "position"
   end
 
 end
